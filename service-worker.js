aef70b8c3acead887d08b7e92e08531cd1450e91
tiasapetsitter-collab <tiasapetsitter@gmail.com>
2025-09-04 14:56:47 -0300

    Add files via upload
    Signed-off-by: tiasapetsitter-collab <tiasapetsitter@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('bem-estar-v5').then(cache=>cache.addAll(['index.html','manifest.json','icon-192.png','icon-512.png'])));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('bem-estar-v4').then(cache => cache.addAll(['index.html','manifest.json','icon-192.png','icon-512.png'])));
 });
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
