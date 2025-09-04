24f428d508dcde1676bdc1d7304701e020d4ed38
tiasapetsitter-collab <tiasapetsitter@gmail.com>
2025-09-04 12:04:26 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,3 @@
+
+self.addEventListener('install', e=>{e.waitUntil(caches.open('bem-estar-v1').then(cache=>cache.addAll(['index.html','manifest.json','icon-192.png','icon-512.png'])));});
+self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
